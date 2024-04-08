d9afa557b2bf16bdad268fe6302e6119b7b46d39
Pmoranga <pmoranga@gmail.com>
2024-04-08 17:13:27 -0300

    Use our defind image

--- modules/ecs_alb/main.tf
+++ modules/ecs_alb/main.tf
@@ -105,7 +105,7 @@ module "ecs_service" {
   # Container definition(s)
   container_definitions = {
     (local.container_name) = {
-      image = var.image
+      image = var.app_image
 
       cpu    = var.app_cpu
       memory = var.app_memory
